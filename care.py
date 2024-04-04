c2b00baf48da32e8214592af56c8516a1e4a4ee4
hillysegal1 <166023524+hillysegal1@users.noreply.github.com>
2024-04-04 20:58:47 +0300

    Create care.py

---
+++ care.py
@@ -0,0 +1,26 @@
+%%writefile care.py
+import streamlit as st
+def to_care():
+  st.session_state['current_page'] ='plant_care'
+
+def main():
+  if st.button("Back"):
+      to_care()
+  st.markdown("""
+      <h1 style='text-align: center; color: green;'>Care for your Plant</h1>
+      <h3 style='text-align: center; color: green;'>Enter your plant’s name to learn how to care for it</he>
+      """, unsafe_allow_html=True)
+  user_input = st.text_input("Enter your plants name:")
+
+  if st.button('Submit'):
+      # Display the submitted text
+      st.session_state['shared_string'] = "hi"
+      st.session_state['p_name'] = user_input
+      st.session_state['current_page'] = 'how_to'
+  image_path = "/content/im.png"
+  st.image(image_path)
+if __name__ == '__main__':
+  main()
+
+
+
