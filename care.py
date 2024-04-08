4fd2cd5beaa9589538ddc1a8bcfc2ab852cfc3fc
hillysegal1 <166023524+hillysegal1@users.noreply.github.com>
2024-04-08 11:43:37 +0300

    Update care.py

--- care.py
+++ care.py
@@ -6,6 +6,7 @@ def to_care():
 def main():
   if st.button("Back"):
       to_care()
+      st.rerun()
   st.markdown("""
       <h1 style='text-align: center; color: green;'>Care for your Plant</h1>
       <h3 style='text-align: center; color: green;'>Enter your plant’s name to learn how to care for it</he>
@@ -17,6 +18,7 @@ def main():
       st.session_state['shared_string'] = "hi"
       st.session_state['p_name'] = user_input
       st.session_state['current_page'] = 'how_to'
+      st.rerun()
   image_path = "im.png"
   st.image(image_path)
 if __name__ == '__main__':
